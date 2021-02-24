23058a3384727dd983315f09a906a4971e0fd68e
truexinology <truexin@163.com>
2021-02-24 18:18:11 +0800

    fix: test1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
+<!--
+ * @Project: 
+ * @Remark: 
+ * @Author: 邹新许
+ * @Date: 2021-02-24 11:02:25
+ * @LastEditors: 邹新许
+ * @LastEditTime: 2021-02-24 18:17:34
+ * @FilePath: /tauri-demo/src/App.vue
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js 33App"/>
   </div>
 </template>
 
